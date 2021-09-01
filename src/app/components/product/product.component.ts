0329c12380107ada8798b5e9c2288063f1d1fafa
= <=>
2021-09-01 11:15:54 +0300

    ürün listeleme çalışması

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  product1={
+    productId:1,
+    productName:"Bardak",
+    categoryId:1,
+    unitPrice:5,
+};
+  product2={
+    productId:2,
+    productName:"Kaşık",
+    categoryId:2,
+    unitPrice:2,
+  };
+  product3={
+    productId:2,
+    productName:"Tabak",
+    categoryId:1,
+    unitPrice:10,
+  };
+  products=[
+    this.product1,
+    this.product2,
+    this.product3,
+  ];
   constructor() { }
 
   ngOnInit(): void {
